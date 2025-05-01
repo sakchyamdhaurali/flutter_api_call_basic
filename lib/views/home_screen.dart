220b843b2521966fac323fff8abd712950fb53aa
dhaurali <sakchyamdhaurali5@gmail.com>
2025-05-01 09:19:07 +0545

    apiCall function created

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,34 +1,52 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_api_call_basic/models/model_sample.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
-const HomeScreen({ Key? key }) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<RandomAPI> postList = [];
+  Future<List<RandomAPI>> getPostAPI() async {
+    final response = await http.get(
+      Uri.parse("https://jsonplaceholder.typicode.com/posts"),
+    );
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map<String, dynamic> i in data) {
+        postList.add(RandomAPI.fromJson(i));
+      }
+      return postList;
+    } else {
+      return postList;
+    }
+  }
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("API CALL",
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 18,
-          fontWeight: FontWeight.w800,
-        ),
+        title: Text(
+          "API CALL",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.w800,
+          ),
         ),
         centerTitle: true,
         backgroundColor: Color(0xFF008000),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(child: Text("Calling API")),
-
-        ],
+        children: [Center(child: Text("Calling API"))],
       ),
     );
   }
