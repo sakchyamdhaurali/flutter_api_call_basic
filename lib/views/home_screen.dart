e8eb20c486b68fe77f2175f52c4e435ffff14e3f
dhaurali <sakchyamdhaurali5@gmail.com>
2025-05-01 09:44:32 +0545

    api called

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -46,7 +46,48 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [Center(child: Text("Calling API"))],
+        children: [
+          Center(child: Text("Calling API")),
+          Expanded(
+            child: FutureBuilder(
+              future: getPostAPI(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Text("Loading");
+                } else {
+                  return ListView.builder(
+                    itemCount: postList.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                         
+                            Text("ID ${postList[index].id}", style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),),
+
+                            Text("Title", style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                            ),),
+                            Text(postList[index].title.toString()),
+                          
+                          
+                          ],
+                                                ),
+                        ));
+                    },
+                  );
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
